3fda5024797324f7fbb67514a08a52c2b9334f83
Daniel Einars <contact@dle.dev>
2020-05-12 22:44:49 +0200

    switched back to url-loader for loading files

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,14 @@ module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
           {
             test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
             exclude: /node_modules/,
-            use: ["file-loader?name=[name].[ext]"], // ?name=[name].[ext] is only necessary to preserve the original file name
+            use: [
+              {
+                loader: "url-loader",
+                options: {
+                  limit: 500,
+                },
+              },
+            ],
           },
         ],
       },
