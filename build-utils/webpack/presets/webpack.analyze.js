2d68d5224eb35d85ee9e539744a6caa04c1ff6cc
Daniel Einars <contact@dle.dev>
2020-05-16 16:52:53 +0200

    using TS for webpack config

--- build-utils/webpack/presets/webpack.analyze.js
+++
@@ -1,7 +0,0 @@
-const WebpackBundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-const analyze = () => ({
-  plugins: [new WebpackBundleAnalyzer()],
-});
-
-module.exports = analyze;
