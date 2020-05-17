2d668a5a7a050206369ae4d8ce10a5c5915abfde
Daniel Einars <contact@dle.dev>
2020-05-17 16:16:37 +0200

    separate build docker file

---
+++ docker-build.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -e
+
+function npmVersion() {
+  node <<EOJS
+  var pjson = require('./package.json');
+  console.log(pjson.version);
+EOJS
+}
+
+function npmName() {
+  node <<EOJS
+  var pjson = require('./package.json');
+  console.log(pjson.name);
+EOJS
+}
+
+if [[ $(git rev-parse --abbrev-ref HEAD) == master ]]; then
+
+  echo "***************************************************"
+  echo "************* BUILD AND DEPLOY PROD ***************"
+  echo "***************************************************"
+
+  HOST="eu.gcr.io/dle-dev"
+  NAME=$(npmName)
+  VERSION=$(npmVersion)
+
+  TAG=${HOST}/${NAME}
+  TAG_LATEST=${TAG}:latest
+  TAG_VERSION=${TAG}:${VERSION}
+
+  echo "$TAG_LATEST"
+  echo "$TAG_VERSION"
+
+  rm -rf ./dist
+  yarn run build:prod
+
+  docker build . -t "${TAG_LATEST}"
+  docker tag "${TAG_LATEST}" "${TAG_VERSION}"
+
+
+else
+  echo "***************************************************"
+  echo "************ BUILD AND DEPLOY STAGING *************"
+  echo "***************************************************"
+
+  HOST="eu.gcr.io/dle-dev"
+  NAME=$(npmName)
+  VERSION=$(npmVersion)
+
+  TAG=${HOST}/${NAME}
+  TAG_LATEST=${TAG}:staging
+
+  rm -rf ./dist
+  yarn run build:staging
+
+  docker build . -t "${TAG_LATEST}"
+fi
