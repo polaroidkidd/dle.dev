43df62eb714b038ee6a3a8b138310f9187dafe78
Daniel Einars <contact@dle.dev>
2020-10-04 23:13:42 +0200

    ts-ignore for now
    Signed-off-by: Daniel Einars <contact@dle.dev>

--- src/app/components/loading-indicator/loading-indicator.tsx
+++ src/app/components/loading-indicator/loading-indicator.tsx
@@ -4,6 +4,7 @@ import Loader from 'react-loader-spinner';
 
 const LoadingIndicator = () => {
   return (
+  // @ts-ignore
     <div className={style.loadingIndicator}>
       <Loader type="Oval" color={'#ADADAD'} width={100} height={100} />
     </div>
