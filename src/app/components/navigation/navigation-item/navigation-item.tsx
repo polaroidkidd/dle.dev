9e1af532a1fc49f12ebc46ae235e7c5a1744ef02
Daniel Einars <contact@dle.dev>
2020-11-02 00:01:43 +0100

    removed classnames
    Signed-off-by: Daniel Einars <contact@dle.dev>

--- src/app/components/navigation/navigation-item/navigation-item.tsx
+++ src/app/components/navigation/navigation-item/navigation-item.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './navigation-item.scss';
-import classNames from 'classnames';
 
 type Props = {
   description: 'vitae' | 'portfolio' | 'blog' | 'contact';
@@ -8,15 +7,10 @@ type Props = {
   handleActiveNavigation: (e: React.MouseEvent<HTMLDivElement>) => void;
 };
 
-const NavigationItem: React.FC<Props> = ({
-  description,
-  isActive,
-  handleActiveNavigation,
-  children,
-}) => {
+const NavigationItem: React.FC<Props> = ({ description, isActive, handleActiveNavigation, children }) => {
   return (
     <div
-      className={classNames('nav-item-container', isActive ? 'nav-item-container__active' : '')}
+      className={`'nav-item-container' ${isActive && 'nav-item-container__active'}`}
       onClick={handleActiveNavigation}
     >
       <div className="nav-item-container--icon">{children}</div>
