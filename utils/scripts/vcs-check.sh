06b8a425529e5ee49ff5137e63ca915f84a40f85
Daniel Einars <contact@dle.dev>
2020-12-03 10:01:35 +0100

    add config path flag for commitlint

--- utils/scripts/vcs-check.sh
+++ utils/scripts/vcs-check.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-if [[ $(git rev-parse --abbrev-ref HEAD) == master ]]; then commitlint -E HUSKY_GIT_PARAMS; fi
+if [[ $(git rev-parse --abbrev-ref HEAD) == master ]]; then commitlint --config ./utils/config/commitlint.config.js -E HUSKY_GIT_PARAMS; fi
