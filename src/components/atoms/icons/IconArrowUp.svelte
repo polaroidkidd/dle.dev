ec6956f100134cc6cd8608c1524ffaf06140b295
Daniel Einars <contact@dle.dev>
2023-04-25 23:17:36 +0200

    feat: implement scroll-back-up-error
    Signed-off-by: Daniel Einars <contact@dle.dev>

---
+++ src/components/atoms/icons/IconArrowUp.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import classNames from "classnames";
+
+	let clazz = "";
+	export { clazz as class };
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="52"
+	height="52"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="1"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	class={classNames("feather feather-arrow-up", clazz)}
+>
+	<line x1="12" y1="19" x2="12" y2="5" />
+	<polyline points="5 12 12 5 19 12" />
+</svg>
