7d9f8cadcc577deeb46cc0b3e7961acd07194764
Daniel Einars <contact@dle.dev>
2024-04-08 11:22:36 +0200

    remove iOS capture camera attribute

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
 	import Typography from '@components/atoms/typography/typography.svelte';
 	import IconLoading from '@components/atoms/icons/IconLoading.svelte';
 	import ArticleCards from '@components/molecules/articleCards.svelte';
-	import classNames from 'classnames';
 
 	export let data: PageData;
 </script>
@@ -13,37 +12,6 @@
 <div class=" w-full flex flex-col items-center">
 	<Typography variant="h2" size="2xl" align="center">Latest articles</Typography>
 	<div class="container max-w-screen-sm pb-10 flex flex-col justify-center items-stretch pt-10">
-		<a href={`iosCameraAttribute`}>
-			<div
-				class={classNames(
-					'mb-12',
-					'p-4',
-					'mx-4',
-					'transition-all cursor-pointer',
-					'bg-white',
-					'flex flex-col justify-center items-start',
-					'relative',
-					'border',
-					'rounded',
-					'border-red-300',
-					'drop-shadow-md',
-					'hover:drop-shadow-lg',
-					'border-x',
-
-					// dark
-					'dark:hover:border-red-200 dark:bg-neutral-800',
-					'dark:border-transparent',
-					'dark:hover:border-red-300',
-					'dark:bg-neutral-800'
-				)}
-			>
-				<div class="text-lg">iOS Camera Attribute Showcase</div>
-				<div class={classNames('p-1', 'text-xs', 'absolute -top-7 -right-0.5')}>
-					{new Date().toDateString()}
-				</div>
-			</div>
-		</a>
-
 		{#if data.blogsMetaData !== undefined}
 			<ArticleCards blogsMetaData={data.blogsMetaData} />
 		{:else if data.streamed !== undefined}
